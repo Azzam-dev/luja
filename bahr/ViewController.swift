94fdc796fec2970d9a81e9b1cd1578bfc9a003bd
Azzam R Alrashed <azzam.rar@gmail.com>
2022-12-14 18:56:21 +0300

    worked on setting up the first ViewController

--- bahr/ViewController.swift
+++ bahr/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("يشتغل الامر هذا بعد تحميل الصفحة التعريفية")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(4)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print(3)
     }
 
 
